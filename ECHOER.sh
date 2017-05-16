f1aa4093634cde4d6d6861fee30b7479a1005c35
theochemtheo <theo@theokeane.net>
2017-05-16 16:41:04 +0100

    Adding in ECHOER.sh, chmod +x for some .sh

---
+++ ECHOER.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+# Usage: echoer.sh [total number of lines per iteration] [output name]
+
+# delete previous output
+rm -f $2
+
+# Initialize counter
+counter=1
+
+# Loop through the files that match the pattern
+for i in $(cat elist.lst); do
+	# For the first loop, check how long the matching files are
+	while [ $counter == 1 ]; do
+		# Number of lines for the matching file
+		NLINES=$( wc -l < $i )
+		# Calculate number of newlines required
+		REPLINES=$(($1-$NLINES))
+		# Expand this
+		REPS=$(eval echo {1..$REPLINES})
+		# Increment the counter
+		((counter++))
+	done
+	cat $i >> $2
+	printf '\n%.0s' $REPS >> $2
+	echo $i
+done
