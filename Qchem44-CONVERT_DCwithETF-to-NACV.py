c73ab8055199c866d0b6b1a9b718e726f4e0f7b3
theochemtheo <theo@theokeane.net>
2017-08-17 18:26:42 +0100

    Improved the comments

--- Qchem44-CONVERT_DCwithETF-to-NACV.py
+++ Qchem44-CONVERT_DCwithETF-to-NACV.py
@@ -10,11 +10,13 @@ from numpy import linalg as LA
 np.set_printoptions(linewidth=160)
 
 
+# Function for obtaining the absolute energy of state i
 def getEXE(i):
     EXEi = np.float64(re.findall('(?<=Total energy for state{0:4}:).*'.format(i), open(args["file"]).read())[0])
     return EXEi
 
 
+# Set up argparse
 parser = argparse.ArgumentParser(description="This script is for converting Qchem 4.4 derivative couplings (d_JK) to non-adiabatic couplings (h_JK)")
 parser.add_argument("-i", dest="file", metavar="input", help="the Qchem 4.4 output file", required=True)
 parser.add_argument("-J", dest="Sone", metavar="<state J>", help="the first state, J, of h_JK", type=int, required=True)
@@ -75,19 +77,26 @@ with open(args["file"]) as fh:
                 # remove the newline characters and keep only the first 4 columns (NAtom, X, Y, Z), as a list of strings
                 allcouplingtext.append((next(fh).rstrip()).split()[0:4])
 # Once the relevant part of the file has been extracted, clean it up into a 4 column (NAtom, X, Y, Z) np array of floats and put into individual arrays
-DCnoETF = np.array(allcouplingtext[10:10 + natoms]).astype('f')
+# DC no ETF: the derivative coupling vector, d_JK = < Psi_J | d/dR | Psi_K > = h_JK / (E_J - E_K). This is unused at this time
+# DCnoETF = np.array(allcouplingtext[10:10 + natoms]).astype('f')
+# NACV: the non-adiabatic coupling vector, h_JK, = < Psi_J | dH/dR | Psi_K >. This is usually quite low precision because the output formatting is '-0.6f'
 NACV = np.array(allcouplingtext[15 + natoms: 15 + natoms * 2]).astype('f')
+# DCwithETF: the derivative coupling vector, d_JK, including the electron-ranslation factor correction. See J.Chem.Phys., 2011, 135, 234105.
 DCwithETF = np.array(allcouplingtext[20 + natoms * 2: 20 + natoms * 3]).astype('f')
 
+# Re-calculate the NACV using the DCwithETF and delta E to recover a higher precision number
 calcNACV = np.multiply(DCwithETF[:, 1:4], EdiffJK)
 
+# Output the magnitudes to the screen
 print("The NACV magnitude obtained directly from the file is:             {:+.4e}".format(LA.norm(NACV[:, 1:4], 'fro')))
 print("The NACV magnitude as derived from the DC with ETF and delta E is: {:+.4e}".format(LA.norm(calcNACV, 'fro')))
 
+# If the optional condition is set, print the whole NACV to the screen
 if args["show"] is True:
     print("The NACV derived from the DC with ETF and delta E is:")
     print("{}".format(calcNACV))
 
+# If the optional condition is set, print the whole NACV to the desired file
 if args["outp"] is not None:
     print("The calculated NACV will be printed to file: {}".format(args["outp"]))
     np.savetxt(args["outp"], calcNACV, fmt="%+.6e", delimiter='\t')
