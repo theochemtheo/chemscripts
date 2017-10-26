76027a35130a2210d462e520dd896ef5cd86b52e
theochemtheo <theo@theokeane.net>
2017-10-26 18:51:23 +0100

    Adding example escf data extraction script

---
+++ TM72-EXTRACT_excitations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This script is intended to provide examples of ways of getting excitations out of the output from a Turbomole 7.2 escf calculation
+"""
+
+import argparse
+import numpy as np
+import re
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("escffile", help="name of escf output file")
+args = parser.parse_args()
+
+excitations = np.loadtxt(re.findall('(?<=Excitation energy \/ cm\^\(-1\)\:).*', open(args.escffile).read()))
+velrep = np.loadtxt(re.findall('(?<=Oscillator strength\:\n\n    velocity representation\:).*', open(args.escffile).read()))
+lenrep = np.loadtxt(re.findall('(?<=length representation\:).*', open('escf.out').read()))[0::2]
+mixedrep = np.loadtxt(re.findall('(?<=mixed representation\:).*', open(args.escffile).read()))
+
+print("{}".format(excitations))
+print("{}".format(velrep))
+print("{}".format(lenrep))
+print("{}".format(mixedrep))
