8104fff6358fe1a4587a1dba02a16170900bc7a6
theochemtheo <theo@theokeane.net>
2018-03-01 14:55:57 +0000

    added lifetime to parseTURBO + rename functions

--- pyfuncs/parseTURBO.py
+++ pyfuncs/parseTURBO.py
@@ -2,21 +2,36 @@ from numpy import loadtxt
 from re import findall
 
 
-def extractexcitations(file):
+def excitations(file):
     excitations = loadtxt(findall('(?<=Excitation energy \/ cm\^\(-1\)\:).*', open(file).read()))
     return excitations
 
 
-def extractvelrep(file):
-    velrep = loadtxt(findall('(?<=Oscillator strength\:\n\n    velocity representation\:).*', open(file).read()))
+def oscvelrep(file):
+    velrep = loadtxt(findall('(?<=velocity representation\:).*', open(file).read()))[0::3]
     return velrep
 
 
-def extractlenrep(file):
+def osclenrep(file):
     lenrep = loadtxt(findall('(?<=length representation\:).*', open(file).read()))[0::3]
     return lenrep
 
 
-def extractmixrep(file):
+def oscmixrep(file):
     mixedrep = loadtxt(findall('(?<=mixed representation\:).*', open(file).read()))[0::2]
     return mixedrep
+
+
+def taulenrep(file):
+    lenrep = loadtxt(findall('(?<=length representation\:).*', open(file).read()))[1::3]
+    return lenrep
+
+
+def taumixrep(file):
+    mixedrep = loadtxt(findall('(?<=mixed representation\:).*', open(file).read()))[1::2]
+    return mixedrep
+
+
+def tauvelrep(file):
+    velrep = loadtxt(findall('(?<=velocity representation\:).*', open(file).read()))[1::3]
+    return velrep
