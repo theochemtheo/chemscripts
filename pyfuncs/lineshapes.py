9adadbf106b6c3b5e8e06c754a743ccb6edeeed6
theochemtheo <theo@theokeane.net>
2018-03-02 18:04:09 +0000

    improved lineshapes, fixed parseTURBO 1C vs 2C

--- pyfuncs/lineshapes.py
+++ pyfuncs/lineshapes.py
@@ -1,33 +1,67 @@
 from numpy import pi, exp, log
 
 
+def _IRprefac(strength, FWHM):
+    HWHM = FWHM / 2
+    prefac = ((100 / log(10) / pi * strength) / HWHM)
+    return prefac
+
+
+def _UVprefac(strength, FWHM):
+    prefac = (2.175e8) * (strength / FWHM)
+    return prefac
+
+
+def _emprefac(x, strength, FWHM):
+    prefac = x**3 * strength / FWHM
+    return prefac
+
+
+def _Lorentzian(x, band, FWHM):
+    HWHM = FWHM / 2
+    probability = 1 / (1 + ((x - band) / HWHM)**2)
+    return probability
+
+
+def _Gaussian(x, band, FWHM):
+    probability = exp(-2.772 * ((x - band) / FWHM)**2)
+    return probability
+
+
 def IRLorentzian(x, band, strength, FWHM):
     # Lorentzian lineshape function with IR prefactor
     # This is based on a document by Jens Spanget-Larsen, which may be found at dx.doi.org/10.13140/RG.2.1.4181.6160
-    HWHM = FWHM / 2
-    bandshape = ((100 / log(10) / pi * strength) / HWHM) / (1 + ((x - band) / HWHM)**2)
-    return bandshape
+    epsilon = _IRprefac(strength, FWHM) * _Lorentzian(x, band, FWHM)
+    return epsilon
 
 
 def IRGaussian(x, band, strength, FWHM):
     # Gaussian lineshape function with IR prefactor
-    HWHM = FWHM / 2
-    bandshape = ((100 / log(10) / pi * strength) / HWHM) * exp(-2.772 * ((x - band) / FWHM)**2)
-    return bandshape
+    epsilon = _IRprefac(strength, FWHM) * _Gaussian(x, band, FWHM)
+    return epsilon
 
 
 def UVLorentzian(x, band, strength, FWHM):
     # Lorentzian lineshape function with UV prefactor
     # This is based on a document by Jens Spanget-Larsen, which may be found at dx.doi.org/10.13140/RG.2.1.4181.6160
-    HWHM = FWHM / 2
-    bandshape = (2.175e8) * (strength / FWHM) / (1 + ((x - band) / HWHM)**2)
-    return bandshape
+    epsilon = _UVprefac(strength, FWHM) * _Lorentzian(x, band, FWHM)
+    return epsilon
 
 
 def UVGaussian(x, band, strength, FWHM):
     # Gaussian lineshape function with UV prefactor
-    bandshape = (2.175e8) * (strength / FWHM) * exp(-2.772 * ((x - band) / FWHM)**2)
-    return bandshape
+    epsilon = _UVprefac(strength, FWHM) * _Gaussian(x, band, FWHM)
+    return epsilon
+
+
+def emLorentzian(x, band, strength, FWHM):
+    '''
+    This provides a Lorentzian lineshape for emission which is scaled in arbitrary units apart from
+    the fact that the emission intensity should be cubically dependent on the transition energy
+    This is due to the Einstein spontaneous emission coefficient, A.
+    '''
+    epsilon = _emprefac(x, strength, FWHM) * _Lorentzian(x, band, FWHM)
+    return epsilon
 
 
 def emGaussian(x, band, strength, FWHM):
@@ -36,5 +70,5 @@ def emGaussian(x, band, strength, FWHM):
     the fact that the emission intensity should be cubically dependent on the transition energy
     This is due to the Einstein spontaneous emission coefficient, A.
     '''
-    bandshape = x**3 * strength / FWHM * exp(-2.772 * ((x - band) / FWHM)**2)
-    return bandshape
+    epsilon = _emprefac(x, strength, FWHM) * _Gaussian(x, band, FWHM)
+    return epsilon
--- pyfuncs/parseTURBO.py
+++ pyfuncs/parseTURBO.py
@@ -1,4 +1,4 @@
-from numpy import loadtxt
+from numpy import loadtxt, nan
 from re import findall
 
 
@@ -7,31 +7,103 @@ def excitations(file):
     return excitations
 
 
-def oscvelrep(file):
+def twoCcheck(file):
+    istwoC = False
+    with open(file, 'r') as f:
+        for line in f.readlines():
+            if 'Two-component modus switched on ! ' in line:
+                istwoC = True
+    return istwoC
+
+
+def _oneCoscvelrep(file):
+    velrep = loadtxt(findall('(?<=velocity representation\:).*', open(file).read()))[0::2]
+    return velrep
+
+
+def _oneCosclenrep(file):
+    lenrep = loadtxt(findall('(?<=length representation\:).*', open(file).read()))[0::2]
+    return lenrep
+
+
+def _oneCoscmixrep(file):
+    mixedrep = loadtxt(findall('(?<=mixed representation\:).*', open(file).read()))[0::1]
+    return mixedrep
+
+
+def _twoCoscvelrep(file):
     velrep = loadtxt(findall('(?<=velocity representation\:).*', open(file).read()))[0::3]
     return velrep
 
 
-def osclenrep(file):
+def _twoCosclenrep(file):
     lenrep = loadtxt(findall('(?<=length representation\:).*', open(file).read()))[0::3]
     return lenrep
 
 
-def oscmixrep(file):
+def _twoCoscmixrep(file):
     mixedrep = loadtxt(findall('(?<=mixed representation\:).*', open(file).read()))[0::2]
     return mixedrep
 
 
-def taulenrep(file):
+def _twoCtaulenrep(file):
     lenrep = loadtxt(findall('(?<=length representation\:).*', open(file).read()))[1::3]
     return lenrep
 
 
-def taumixrep(file):
+def _twoCtaumixrep(file):
     mixedrep = loadtxt(findall('(?<=mixed representation\:).*', open(file).read()))[1::2]
     return mixedrep
 
 
-def tauvelrep(file):
+def _twoCtauvelrep(file):
     velrep = loadtxt(findall('(?<=velocity representation\:).*', open(file).read()))[1::3]
     return velrep
+
+
+def oscvelrep(file):
+    if twoCcheck(file):
+        velrep = _twoCoscvelrep(file)
+    else:
+        velrep = _oneCoscvelrep(file)
+    return velrep
+
+
+def osclenrep(file):
+    if twoCcheck(file):
+        lenrep = _twoCosclenrep(file)
+    else:
+        lenrep = _oneCosclenrep(file)
+    return lenrep
+
+
+def oscmixrep(file):
+    if twoCcheck(file):
+        mixrep = _twoCoscmixrep(file)
+    else:
+        mixrep = _oneCoscmixrep(file)
+    return mixrep
+
+
+def tauvelrep(file):
+    if twoCcheck(file):
+        velrep = _twoCtauvelrep(file)
+    else:
+        velrep = nan
+    return velrep
+
+
+def taulenrep(file):
+    if twoCcheck(file):
+        lenrep = _twoCtaulenrep(file)
+    else:
+        lenrep = nan
+    return lenrep
+
+
+def taumixrep(file):
+    if twoCcheck(file):
+        mixrep = _twoCtaumixrep(file)
+    else:
+        mixrep = nan
+    return mixrep
